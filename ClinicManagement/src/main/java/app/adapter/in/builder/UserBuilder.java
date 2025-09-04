9e615942106c46aa93a5e8da612ed46b52e72fd1
oscardcd <oscardcd17@gmail.com>
2025-09-04 18:21:19 -0500

    comment error on user builder

--- ClinicManagement/src/main/java/app/adapter/in/builder/UserBuilder.java
+++ ClinicManagement/src/main/java/app/adapter/in/builder/UserBuilder.java
@@ -5,6 +5,7 @@
 package app.adapter.in.builder;
 
 import app.adapter.in.validators.UserValidator;
+import app.domain.models.User;
 
 /**
  *
@@ -15,5 +16,15 @@ public class UserBuilder {
 
 //	@Autowired
     private UserValidator userValidator;
+
+    public UserBuilder build(String name, String document, String age, String userName, String password) throws Exception {
+        User user = new User();
+        user.setfullName(userValidator.nameValidator(name));
+//        user.setDoc (userValidator.documentValidator(document));
+//        user.setAge(userValidator.ageValidator(age));
+//        user.setUserName(userValidator.userNameValidator(userName));
+//        user.setPassword(userValidator.passwordValidator(password));
+//        return user;
+    }
 
 }
