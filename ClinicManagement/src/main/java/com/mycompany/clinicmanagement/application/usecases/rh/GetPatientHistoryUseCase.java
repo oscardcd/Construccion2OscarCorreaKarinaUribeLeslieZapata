28e08330012762f887ba07984120fa62e54f8d1d
lelizapata <stefazapata4212@gmail.com>
2025-10-03 18:13:38 -0500

    Use case correct

--- ClinicManagement/src/main/java/com/mycompany/clinicmanagement/application/usecases/rh/GetPatientHistoryUseCase.java
+++ ClinicManagement/src/main/java/com/mycompany/clinicmanagement/application/usecases/rh/GetPatientHistoryUseCase.java
@@ -1,26 +1,25 @@
 package com.mycompany.clinicmanagement.application.usecases.rh;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import app.domain.models.MedicalRecord;
+import app.domain.models.MedicalVisit;
 
 //Consulta la historia clínica de un paciente.
 public class GetPatientHistoryUseCase {
-    
+
     private final MedicalRecord medicalRecord;
 
-    public GetPatientHistoryUseCase(MedicalRecord medicalRecord;) {
+    public GetPatientHistoryUseCase(MedicalRecord medicalRecord) {
         this.medicalRecord = medicalRecord;
     }
 
-    //Arroja una lista de todas las visitas que ha tenido 
-    public List<MedicalRecord> execute(String patientId) {
-        return medicalRecord.getVisits(patientId);
+    //Retorna una lista con todas las visitas que ha tenido el paciente
+    public List<MedicalVisit> execute(String patientId) {
+        if (medicalRecord == null || !medicalRecord.getPatientId().equals(patientId)) {
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(medicalRecord.getVisits().values());
     }
-
 }
-
-
-
-
-
-
